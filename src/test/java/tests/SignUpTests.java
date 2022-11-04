09a00d193786d668a6f485a6f40b0e61f996db34
Berkan <bsaridilek@gmail.com>
2022-11-04 13:16:57 -0700

    SignUpPage, SignUpTests added.

---
+++ src/test/java/tests/SignUpTests.java
@@ -0,0 +1,29 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.SignUpPage;
+import utils.SeleniumUtils;
+
+public class SignUpTests extends TestBase{
+
+
+    @Test
+    public void signUpPositive(){
+
+
+        SignUpPage signUpPage = new SignUpPage();
+
+        signUpPage.signUpPageLink.click();
+        signUpPage.enterBasicInfoRandom();
+        signUpPage.enterContactInfoRandom();
+        signUpPage.enterLoginInfoRandom();
+
+        SeleniumUtils.waitFor(1);
+        Assert.assertTrue(driver.getTitle().equals("Personal Details : Delta Air Lines"));
+
+
+    }
+
+
+}
