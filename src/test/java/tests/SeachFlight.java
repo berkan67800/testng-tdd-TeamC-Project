539c25d7d80404b8701d9768f6d35c565a46982f
Berkan <bsaridilek@gmail.com>
2022-11-07 14:08:16 -0800

    HomePage updated, SearchFlight fixed.

---
+++ src/test/java/tests/SeachFlight.java
@@ -0,0 +1,39 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import utils.SeleniumUtils;
+
+public class SeachFlight extends TestBase{
+
+    @Test
+    public void seachFlightNegativeNoEntry(){
+
+        HomePage homePage = new HomePage();
+
+        homePage.flightSearchButton.click();
+        Assert.assertTrue(homePage.searchErrorMessage.isDisplayed());
+    }
+
+
+     @Test
+    public void seachFlightPositive(){
+
+        HomePage homePage = new HomePage();
+
+         homePage.selectFromTo("LAX","JFK");
+         homePage.selectDropDownItem("One Way", homePage.tripTypeDropDown,  homePage.tripTypes);
+         homePage.selectDate();
+         homePage.selectDropDownItem("2",homePage.passengersDropDown,homePage.passengerAmount);
+         SeleniumUtils.jsClick(homePage.flightSearchButton);
+         SeleniumUtils.waitFor(2);
+         Assert.assertTrue(driver.getCurrentUrl().contains("/flight-search/search-results"));
+
+    }
+
+
+
+
+}
