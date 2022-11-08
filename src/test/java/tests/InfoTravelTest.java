e26c1f4f1b1a90d23150ce5a9d40dc852d59ff09
Berkan <bsaridilek@gmail.com>
2022-11-08 13:30:01 -0800

    Bug fixed

--- src/test/java/tests/InfoTravelTest.java
+++ src/test/java/tests/InfoTravelTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import pages.InfoTravel;
+import utils.SeleniumUtils;
 
 public class InfoTravelTest extends TestBase {
 
@@ -14,8 +15,9 @@ public class InfoTravelTest extends TestBase {
         InfoTravel infoTravel=new InfoTravel();
         infoTravel.travelInfo.click();
         infoTravel.trip_Protection.click();
-        Thread.sleep(3000);
-//        Assert.assertTrue(driver.getTitle().toLowerCase().contains("trip protection search: delta air lines"));
+        SeleniumUtils.waitFor(1);
+        System.out.println(driver.getTitle());
+        Assert.assertTrue(driver.getTitle().contains("Delta Air Lines | Flights & Plane Tickets + Hotels & Rental Cars"));
 
     }
 }
